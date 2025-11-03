daac22fc8b16ffd6a97de2e795b1c38d9954d894
dhruv <dhruvkhurana162@gmail.com>
2025-11-03 17:41:11 +0530

    final1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,44 +3,39 @@ package com.driver;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Welcome to Pizza Shop\n");
+        System.out.print("What would u like to Have - N for Normal | D for Deluxe ");
+        String pizzaBase = sc.nextLine();
+        System.out.print("What Normal Pizza do u want - V for Veg | N for Non-veg ");
+        boolean check = false;
+        if (sc.nextLine().equalsIgnoreCase("v")) {
+            check = true;
+        }
 
-      //First of all welcome the customer
-      System.out.print("Welcome to Pizza Shop\n");
+        Pizza p;
+        if (pizzaBase.equalsIgnoreCase("N")) {
+            p = new Pizza(check);
+        } else {
+            p = new DeluxePizza(check);
+        }
 
-      //lets ask if the customer want Normal or Deluxe
-      System.out.print("What would u like to Have - N for Normal | D for Deluxe ");
-      String pizzaBase = sc.nextLine();
+        System.out.print("Do u like to add Extra Cheese - Y for Yes | N for No ");
+        if (sc.nextLine().equalsIgnoreCase("y")) {
+            p.addExtraCheese();
+        }
 
-      //last ask customer if they want veg or non veg
-      System.out.print("What Normal Pizza do u want - V for Veg | N for Non-veg ");
-      boolean check = false;
-      if(sc.nextLine().equalsIgnoreCase("v")) check = true;
+        System.out.print("Do u like to add Extra Topping - Y for Yes | N for No ");
+        if (sc.nextLine().equalsIgnoreCase("y")) {
+            p.addExtraToppings();
+        }
 
-      //lets create a object of what customer want - normal or delaux with veg or non veg type
-      Pizza p;
+        System.out.print("Do u like to Takeout the order - Y for Yes | N for No ");
+        if (sc.nextLine().equalsIgnoreCase("y")) {
+            p.addTakeaway();
+        }
 
-      if(pizzaBase.equalsIgnoreCase("N")) {
-          p = new Pizza(check);
-      }
-      else{
-          p = new DeluxePizza(check);
-      }
-
-      //lets ask customer if they want extra Cheese or not
-      System.out.print("Do u like to add Extra Cheese - Y for Yes | N for No ");
-      if(sc.nextLine().equalsIgnoreCase("y")) p.addExtraCheese();
-
-          //lets ask customer if they want extra topping or not
-          System.out.print("Do u like to add Extra Topping - Y for Yes | N for No ");
-      if(sc.nextLine().equalsIgnoreCase("y")) p.addExtraToppings();
-
-          //takeaway or not
-          System.out.print("Do u like to Takeout the order - Y for Yes | N for No ");
-      if(sc.nextLine().equalsIgnoreCase("y")) p.addTakeaway();
-
-          System.out.print(p.getBill());
-
-  }
+        System.out.print(p.getBill());
+    }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,80 +1,76 @@
 package com.driver;
 
 public class Pizza {
-
     private int price;
     private Boolean isVeg;
     private String bill;
-
     private boolean isCheeseAdded;
     private boolean isToppingadded;
     private boolean takeaway;
 
-    public Pizza(Boolean isVeg){
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
-
-        if(isVeg){
-            this.price+=300;
+        if (isVeg) {
+            this.price += 300;
             this.bill = "Base Price Of The Pizza: 300\n";
-        }
-        else{
-            this.price+=400;
+        } else {
+            this.price += 400;
             this.bill = "Base Price Of The Pizza: 400\n";
         }
-        // your code goes here
+
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
-        if(!this.isCheeseAdded) {
+    public void addExtraCheese() {
+        if (!this.isCheeseAdded) {
             this.price += 80;
             this.isCheeseAdded = true;
         }
+
     }
 
-    public void addExtraToppings(){
-        if(!this.isToppingadded){
-            if(this.isVeg){
-                this.price+=70;
+    public void addExtraToppings() {
+        if (!this.isToppingadded) {
+            if (this.isVeg) {
+                this.price += 70;
                 this.isToppingadded = true;
-            }
-            else{
-                this.price+=120;
+            } else {
+                this.price += 120;
                 this.isToppingadded = true;
             }
         }
+
     }
 
-    public void addTakeaway(){
-        // your code goes here
-        if(!takeaway) {
+    public void addTakeaway() {
+        if (!this.takeaway) {
             this.price += 20;
             this.takeaway = true;
         }
+
     }
 
-    public String getBill(){
-        // your code goes here
-        if(isCheeseAdded){
-            this.bill += "Extra Cheese Added: 80\n";
+    public String getBill() {
+        if (this.isCheeseAdded) {
+            this.bill = this.bill + "Extra Cheese Added: 80\n";
         }
 
-        if(this.isToppingadded){
-            if(this.isVeg){
-                this.bill += "Extra Toppings Added: 70\n";
-            }
-            else{
-                this.bill += "Extra Toppings Added: 120\n";
+        if (this.isToppingadded) {
+            if (this.isVeg) {
+                this.bill = this.bill + "Extra Toppings Added: 70\n";
+            } else {
+                this.bill = this.bill + "Extra Toppings Added: 120\n";
             }
         }
 
-        if(this.takeaway) bill += "Paperbag Added: 20\n";
+        if (this.takeaway) {
+            this.bill = this.bill + "Paperbag Added: 20\n";
+        }
 
-        this.bill += "Total Price: " + this.price;
+        this.bill = this.bill + "Total Price: " + this.price;
         return this.bill;
     }
 }
